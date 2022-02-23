50f725d384769ef01c4a42eef5c7f2c326663614
Kenan Kadic <kkadic@midtownhi.com>
2022-02-23 10:33:57 -0600

    homepage updated again

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,9 @@
       <p class="pink lighten-4 red--text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi deserunt iste laborum molestias odit quia sed similique. Aliquam consequuntur, cumque harum in incidunt nemo obcaecati quo recusandae similique vitae, voluptates!</p>
       <h1 class="display-4">Massive Display</h1>
       <h2 class="display-1">Smaller Display</h2>
+      <p class="headline"> This is a headline</p>
+      <p class="subheading .font-weight-bold">This is a subheading</p>
+      <p class="caption">This is a caption</p>
       </div>
   </v-container>
 </template>

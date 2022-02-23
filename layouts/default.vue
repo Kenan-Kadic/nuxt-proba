5fe207bd3bacae67f2cdba2ce3028c18e9729aec
Kenan Kadic <kkadic@midtownhi.com>
2022-02-23 10:11:31 -0600

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+
+      <v-main>
+        <Nuxt />
+      </v-main>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Kenan's New Page"
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-toolbar app class="red"></v-toolbar>
+<!--  <v-row justify="center" align="center">-->
+<!--    <v-col cols="12" sm="8" md="6">-->
+
+<!--      <v-card>-->
+<!--      </v-card>-->
+<!--    </v-col>-->
+<!--  </v-row>-->
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>

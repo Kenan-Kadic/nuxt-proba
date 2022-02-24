aadba0d6de5399b88d967d31f371ddcc765493b0
Kenan Kadic <kkadic@midtownhi.com>
2022-02-24 09:22:21 -0600

    dashboard.vue updated

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -4,29 +4,26 @@
 
       <v-container class="my-5">
 
-      <v-layout row>
-        <v-flex xs12 md6>
-        <v-btn outline block class="primary">1</v-btn>
-        </v-flex >
-        <v-flex xs4 md2>
-        <v-btn outline block class="primary">2</v-btn>
-        </v-flex >
-        <v-flex xs4 md2>
-        <v-btn outline block class="primary">3</v-btn>
-        </v-flex >
-        <v-flex xs4 md2>
-        <v-btn outline block class="primary">4</v-btn>
-        </v-flex >
-      </v-layout>
-
-      <v-layout row wrap justify-space-between>
-        <v-flex xs4 md3>
-        <v-btn outline block class="error">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-        <v-btn outline block class="error">2</v-btn>
-        </v-flex>
-      </v-layout>
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
+          <v-flex xs12 md6>
+            <div class="caption gray--text">Project Title</div>
+            <div>{{ project.title }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption gray--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption gray--text">Due by</div>
+            <div> {{ project.due }}</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="caption gray--text">Status</div>
+            <div>{{ project.status }}</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
 
       </v-container>
 </div>
@@ -34,10 +31,27 @@
 
 <script>
 export default {
-name: "dashboard"
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'Kenan Kadic', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+.project.complete{
+  border-left: 4px solid #3CD1C2;
+}
+.project.ongoing{
+  border-left: 4px solid orange
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
 </style>
